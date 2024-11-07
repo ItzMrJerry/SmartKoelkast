2ad606078bc82319111b3667cf0145437a5ee00b
jerry Krekels <JerryKrekels@gmail.com>
2024-11-07 14:43:57 +0100

    changes

--- Technologie/TestProject/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ Technologie/TestProject/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b338fa4dafb5288090b3bc93d2ae9b03be9f2a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
